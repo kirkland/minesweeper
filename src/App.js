9915d80aae4e8775b086266d452a50e6064f5d32
Robert Kaufman <rob@groma.com>
2023-04-11 17:55:19 -0400

    Play around making a grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+function Cell(props) {
+  const { state } = props;
+
+  return <div className={`cell cell-${state}`} />;
+}
+
+function Row(props) {
+  const { row } = props;
+
+  return (
+    <div className="row">
+      {row.map((cell, index) => (
+        <Cell state={cell} key={index} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
+  const rows = 10;
+  const columns = 10;
+
+  let board = new Array(10);
+
+  for (let row = 0; row < rows; row++) {
+    board[row] = new Array(columns).fill("unknown");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      {board.map((row, index) => (
+        <Row row={row} key={index} />
+      ))}
     </div>
   );
 }
