4d0882a3ef4de89ac3374bc59e4155aec652eb53
Robert Kaufman <rob@groma.com>
2023-04-12 10:17:26 -0400

    Remove dead code

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,63 +3,6 @@ import { useState } from "react";
 import Row from "./Row";
 import BoardState from "../board-state";
 
-const setAdjacentBombs = (board, coordinates) => {
-  const rows = board.length;
-  const columns = board[0].length;
-
-  [
-    [1, 0],
-    [1, 1],
-    [1, -1],
-    [0, 1],
-    [0, -1],
-    [-1, 1],
-    [-1, 0],
-    [-1, -1],
-  ].forEach((offsets) => {
-    const adjacentCoordinates = [
-      coordinates[0] + offsets[0],
-      coordinates[1] + offsets[1],
-    ];
-
-    if (
-      0 <= adjacentCoordinates[0] &&
-      adjacentCoordinates[0] < rows &&
-      0 <= adjacentCoordinates[1] &&
-      adjacentCoordinates[1] < columns
-    ) {
-      const adjacentCell =
-        board[adjacentCoordinates[0]][adjacentCoordinates[1]];
-
-      adjacentCell.adjacentBombs = adjacentCell.adjacentBombs + 1;
-    }
-  });
-};
-
-const initializeBoard = (rows, columns, bombs) => {
-  let board = [];
-
-  for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
-    const row = [];
-
-    for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
-      row.push({ adjacentBombs: 0, revealed: false });
-    }
-
-    board.push(row);
-  }
-
-  for (let i = 0; i <= bombs; i++) {
-    const bombRow = Math.floor(Math.random() * rows);
-    const bombColumn = Math.floor(Math.random() * columns);
-
-    board[bombRow][bombColumn].bomb = true;
-    setAdjacentBombs(board, [bombRow, bombColumn]);
-  }
-
-  return board;
-};
-
 const cloneBoard = (board) => {
   const newBoard = [];
 
@@ -81,8 +24,6 @@ export default function Board(props) {
   const rows = 9;
   const columns = 9;
   const bombs = 10;
-
-  // const [board, updateBoard] = useState(initializeBoard(rows, columns, bombs));
 
   const [board, updateBoard] = useState(
     BoardState.createInitialBoard(rows, columns, bombs).board
