4794e17bab448d6be7f93006af6f1bd98fdf06dd
Robert Kaufman <rob@groma.com>
2023-05-04 17:42:35 -0400

    Create Game component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,91 +1,16 @@
-import { useState } from "react";
-
 import Row from "./Row";
 import styles from "./Board.module.css";
 import { GameState, Coordinate } from "../game-state";
 
-export default function Board() {
-  const rows = 9;
-  const columns = 9;
-  const bombs = 10;
+type BoardProps = {
+  board: GameState;
+  onClickCell: (button: string, coordinate: Coordinate) => void;
+};
 
-  const revealCells = (cells: Coordinate[]) => {
-    updateBoard((board: GameState) => {
-      let nextRevealed: Coordinate[] = [];
-      const newBoard = board.clone();
-
-      cells.forEach((coordinate) => {
-        nextRevealed = nextRevealed.concat(newBoard.reveal(coordinate));
-      });
-
-      if (nextRevealed.length > 0) {
-        setTimeout(() => {
-          revealCells(nextRevealed);
-        }, 100);
-      }
-
-      return newBoard;
-    });
-  };
-
-  const startNewGame = () => {
-    updateBoard(GameState.createInitialBoard(rows, columns, bombs));
-  };
-
-  const [board, updateBoard] = useState(
-    GameState.createInitialBoard(rows, columns, bombs)
-  );
-
-  const onClickCell = (button: string, coordinate: Coordinate) => {
-    if (board.state() !== "in progress") {
-      return;
-    }
-
-    if (button === "left") {
-      revealCells([coordinate]);
-    } else {
-      updateBoard((board) => {
-        return board.flag(coordinate);
-      });
-    }
-  };
-
-  let message: string;
-
-  if (board.state() === "lost") {
-    message = "Sorry, you lost.";
-  } else if (board.state() === "won") {
-    message = "Hurray, you won!";
-  } else {
-    message = "Best of luck!";
-  }
+export default function Board(props: BoardProps) {
+  const { board, onClickCell } = props;
 
   return (
-    // <>
-    //   <p>{message}</p>
-    //   <div id="game-status">
-    //     <div id="bombs-remaining-container">
-    //       <div id="bombs-remaining">{board.bombsRemaining()}</div>
-    //     </div>
-    //     <div id="new-game-container">
-    //       <button id="new-game" onClick={startNewGame}>
-    //         New Game
-    //       </button>
-    //     </div>
-    //   </div>
-
-    //   <div className={styles.board}>
-    //     {board.rows.map((row, rowIndex) => (
-    //       <Row
-    //         row={row}
-    //         key={rowIndex}
-    //         rowIndex={rowIndex}
-    //         onClickCell={onClickCell}
-    //       />
-    //     ))}
-    //   </div>
-    // </>
-
     <div className={styles.board}>
       {board.rows.map((row, rowIndex) => (
         <Row
