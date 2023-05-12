44234639d5bc292309b42baeb2f4d32df1df4fc6
Robert Kaufman <rob@groma.com>
2023-05-12 10:25:34 -0400

    Fix for selecting text

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -77,6 +77,11 @@ export default function Cell(props: CellProps) {
     resetTouchState();
   };
 
+  const pointerMoveHandler = (event: React.PointerEvent) => {
+    // This is to prevent the user from selecting text
+    event.preventDefault();
+  };
+
   let cellClass = `${styles.cell} `;
   let content: ReactElement = <div />;
 
@@ -103,6 +108,7 @@ export default function Cell(props: CellProps) {
       onPointerUp={pointerUpHandler}
       onPointerLeave={pointerLeaveHandler}
       onPointerCancel={pointerCancelHandler}
+      onPointerMove={pointerMoveHandler}
       ref={cellRef}
     >
       <div className={styles.contentHolder}>{content}</div>
